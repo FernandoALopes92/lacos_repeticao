f8c766c8152bd42d5ce5550e2ae00e432a3fdfbd
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-05-26 21:46:27 -0300

    Exemplo For of

--- for_2.js
+++ for_2.js
@@ -4,6 +4,8 @@ let lista = ["Banana", "Maça", "Pêra", "Manga", "Abacaxi"];
 
 // console.log[lista[1]];
 
-for(let indice = 0; indice < lista.length; indice++){
-    console.log(lista[indice]);
-}
+// for(let indice = 0; indice < lista.length; indice++){
+//     console.log(lista[indice]);
+// }
+
+
